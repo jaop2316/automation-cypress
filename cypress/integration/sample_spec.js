0999ffd092b9d6ee9660e1aa5253030436aca550
jaop2316 <jessikita.ldu@gmail.com>
2019-08-22 17:50:55 -0500

    add more example code

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,4 +2,19 @@ describe ('Login case', function(){
     it('Visits a rollkall site', function(){
         cy.visit('https://rollkallportal-qa.azurewebsites.net/')
     })
+
+    it('Type user credentials', function(){
+        cy.get('[type="email"]').first().focus().type('zuriel+agency@rollkall.com')
+        cy.get('[type="password"]').first().focus().type('test123')
+        cy.contains('Log In').click()
+    })
+
+    it('Submit form', function(){
+        cy.contains('Log In').click()
+    })
+
+    
+    it('Load jobs page', function(){
+        cy.contains('Log In').click()
+    })
 })
