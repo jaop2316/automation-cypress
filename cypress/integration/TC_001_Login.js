bf220b0074fc9cde4b1fc830c5969582b8f8b350
jaop2316 <jessikita.ldu@gmail.com>
2019-08-26 15:13:28 -0500

    Fix validation messages checks

---
+++ cypress/integration/TC_001_Login.js
@@ -0,0 +1,35 @@
+describe ('Login case', function(){
+    it('Visits Agency site', function(){
+        cy.visit('https://rollkallportal-qa.azurewebsites.net/')
+    })
+
+    it('Type user credentials', function(){
+        cy.get('[type="email"]').first().focus().type('zuriel+agency@rollkall.com')
+        cy.get('[type="password"]').first().focus().type('test123')
+    })
+
+    it('Submit form', function(){
+        cy.contains('Log In').click()
+    })
+
+    
+    it('Load jobs page', function(){
+        cy.wait(500)
+        cy.get('.toast-top-right').should('be.empty')
+        cy.get('.container').contains('Jobs')
+    })
+
+    it('Log out', function(){
+        cy.wait(3000)
+        cy.get('.navbar .dropdown.user-options').click()
+        cy
+        .get('.dropdown-menu >li')
+        .each(($el,index,$list) =>{
+             
+            if($el.text() == 'Logout'){
+                cy.get($el).click()
+            }
+        })
+    })
+
+})
--- cypress/integration/TC_002_CreateJob.js
+++ cypress/integration/TC_002_CreateJob.js
@@ -1,4 +1,5 @@
-describe ('Login case', function(){
+describe ('Create a job', function(){
+
     it('Visits Agency site', function(){
         cy.visit('https://rollkallportal-qa.azurewebsites.net/')
     })
@@ -6,30 +7,22 @@ describe ('Login case', function(){
     it('Type user credentials', function(){
         cy.get('[type="email"]').first().focus().type('zuriel+agency@rollkall.com')
         cy.get('[type="password"]').first().focus().type('test123')
-        cy.contains('Log In').click()
     })
 
     it('Submit form', function(){
         cy.contains('Log In').click()
+        cy.get('.toast-error').should('be.hidden')  
     })
 
-    
     it('Load jobs page', function(){
         cy.wait(500)
+        cy.get('.toast-top-right').should('be.empty')
         cy.get('.container').contains('Jobs')
     })
 
-    it('Log out', function(){
-        cy.wait(3000)
-        cy.get('.navbar .dropdown.user-options').click()
-        cy
-        .get('.dropdown-menu >li')
-        .each(($el,index,$list) =>{
-             
-            if($el.text() == 'Logout'){
-                cy.get($el).click()
-            }
-        })
+
+    it('Click on +job button', function(){
+        cy.get('.btn').contains('+ New Job').click()
     })
 
 })
