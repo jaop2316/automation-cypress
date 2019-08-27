4e2b2a5542f8dfb22cf3103da9ff2e5fa9f8f1b1
jaop2316 <jessikita.ldu@gmail.com>
2019-08-27 17:28:51 -0500

    Create job test case code #2

--- cypress/integration/TC_002_CreateJob.js
+++ cypress/integration/TC_002_CreateJob.js
@@ -25,4 +25,70 @@ describe ('Create a job', function(){
         cy.get('.btn').contains('+ New Job').click()
     })
 
+    it('Fill Job Name field', function(){
+        cy.get('label').contains('Job Name').next('input').focus().type('Security VIP')
+    })
+
+    it('Select a client from the list', function(){
+
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'Client') {
+        
+            cy.wrap($el).find('.react-select').click()
+            cy.get('.react-select__menu').contains('Dallas Cowboys').click()
+          } 
+        })
+    })
+
+    it('Select a project from the list', function(){
+
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'Project') {
+            cy.wrap($el).find('.react-select').click()
+            cy.get('.react-select__menu').contains('Dallas Cowboys Game Day').click()
+          } 
+        })
+    })
+
+
+    it('Select a location from the list', function(){
+
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'Location') {
+            cy.wrap($el).find('.react-select').click()
+            cy.get('.react-select__menu').first().click()
+          } 
+        })
+    })
+
+    it('Select the job type', function(){
+
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'What type of job is this?') {
+            cy.wrap($el).find('.react-select').click()
+            cy.get('.react-select__menu').contains('General Security').click()
+          } 
+        })
+    })
+
+    it('Type a job description', function(){
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'Job Description') {
+            cy.wrap($el).find('textarea').focus().type('provide security')
+          } 
+        })
+    })
+
+
+
 })
