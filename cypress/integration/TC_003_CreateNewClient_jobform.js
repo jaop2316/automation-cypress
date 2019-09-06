8deb3d51486ace95e4367b70cd2e29e62e7a3113
jaop2316 <jessikita.ldu@gmail.com>
2019-09-06 09:42:32 -0500

    add project workflow

--- cypress/integration/TC_003_CreateNewClient_jobform.js
+++ cypress/integration/TC_003_CreateNewClient_jobform.js
@@ -15,8 +15,9 @@ describe ('Reset password', function(){
     })
 
     it('Open the create job form and new client modal', function(){
-        // Init the server
-        cy.server()
+      
+        const todaysDate = Cypress.moment().format('MMDDhmmss')
+        const fakeClient = 'ClientQA' + todaysDate
 
         // open the create job form 
         cy.get('.btn').contains('+ New Job').click()
@@ -36,16 +37,47 @@ describe ('Reset password', function(){
         .get('.form-group')
         .each(($el, index, $list) => {
           if ($el.find("label").text() === 'Client Name') {
-            const todaysDate = Cypress.moment().format('MMDDhmmss')
-            const clientfake = 'ClientQA' + todaysDate
-            cy.get('#clientName').type(clientfake)
+            
+            cy.get('#clientName').type(fakeClient)
             cy.wait(500)
           } 
         })
 
         // Click 'Save' button
         cy.get('.modal-footer').find('.pb-button').click()
+
+        cy
+        .get('.form-group')
+        .each(($el, index, $list) => {
+          if ($el.find("label").text() === 'Client') {
+        
+            cy.wrap($el).find('.react-select').click()
+            cy.get('.react-select__menu').contains(fakeClient).click()
+          } 
+        })
         
     
+    })
+
+    it('Create a new project', function(){
+
+          // open the create new project modal
+          cy
+          .get('.form-group')
+          .each(($el, index, $list) => {
+            if ($el.find("label").text() === 'Project') {
+              cy.wrap($el).find('.btn').contains('+ New').click()
+            }
+          })
+
+          // Type project's name 
+          cy
+          .get('.form-horizontal .form-group')
+          .each(($el, index, $list) => {
+            if($el.find("label").text() === 'Project Name:'){
+                cy.wrap($el).find('input').type('Project 1')
+            }
+          })
+
     })
 })
