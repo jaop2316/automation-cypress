3fcb11ce1ddfd056b25087d9c25bccf84bf3b5dd
jaop2316 <jessikita.ldu@gmail.com>
2019-09-18 16:14:22 -0500

    add mocha statements , change the watch command

--- cypress/integration/Agency/TC_001_Login.js
+++ cypress/integration/Agency/TC_001_Login.js
@@ -1,30 +1,35 @@
+/// <reference types ="cypress" />
+
+
 describe ('Login case', function(){
-    it('Visits Agency site and login (Agency user)', function(){
-        // Go to agency site
+
+    beforeEach(() =>{
+
         cy.visit('https://rollkallportal-qa.azurewebsites.net/')
-       
+    })
+
+    afterEach(function() {
+
+        cy.logout()
+      });
+
+    it('Visits Agency site and login (Agency user)', function(){
+
         cy.login('agency','zuriel+agency@rollkall.com','test123')
 
         cy.wait(3000)
-
-        cy.logout()
-
     })
 
     it('Visits Agency site and login (Super admin user)', function(){
         cy.login('admin','zuriel+admin@rollkall.com','test123')
 
         cy.wait(3000)
-
-        cy.logout()
     })
 
     it('Visits Agency site and login (LEA user)', function(){
         cy.login('lea','zuriel+lea@rollkall.com','test123')
 
         cy.wait(3000)
-
-        cy.logout()
     })
 
     
@@ -33,8 +38,8 @@ describe ('Login case', function(){
         cy.login('coordinator','zuriel+coordinator@rollkall.com','test123')
 
         cy.wait(3000)
-
-        cy.logout()
     })
+
+
 
 })
--- cypress/integration/Agency/TC_005_GeneraReport.js
+++ cypress/integration/Agency/TC_005_GeneraReport.js
@@ -1,3 +1,20 @@
 describe ('Generate a report', function(){
     
+    it('Visits Agency site and Login', function(){
+      
+        cy.visit('https://rollkallportal-qa.azurewebsites.net/')
+         
+        cy.login('agency','zuriel+agency@rollkall.com','test123')
+      })
+
+
+      it('Generate a report', function(){
+
+        cy.wait(1000)
+
+        cy.get('.navbar-default .navbar-nav>li').contains('Reports').click()
+
+
+      })
+
 })
