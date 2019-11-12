e416ba4b644359461d121713c6dae04b9477ccff
jaop2316 <jessikita.ldu@gmail.com>
2019-11-12 15:22:42 -0500

    Add variations cases to sign up test case

--- cypress/integration/Agency/TC_004_SignUp.js
+++ cypress/integration/Agency/TC_004_SignUp.js
@@ -3,13 +3,16 @@
 
 describe ('SignUp', function(){
 
-    it('Visits Agency site', function(){
 
-         // Go to the agency site
+  beforeEach(() =>{
     cy.visit('https://rollkallportal-qa.azurewebsites.net/')
-
+    
     // Click on the Sign Up button 
     cy.get('.btn').contains('Sign Up').click()
+
+})
+
+    it('Sign Up (Agency user)', function(){
 
     // Fill the agency name field 
    
@@ -64,8 +67,82 @@ describe ('SignUp', function(){
     cy.contains('Submit').click()
 
 
+    // Variations
+
+    //cy.get('a').contains('Click Here').click()  LEA workflow
+    //cy.url().should('include','signup-lea')
+
+
     })
 
-   
+})
+
+
+describe ('Sign Up case (Variations)', function(){
+
+  beforeEach(() =>{
+    cy.visit('https://rollkallportal-qa.azurewebsites.net/')
+    
+    // Click on the Sign Up button 
+    cy.get('.btn').contains('Sign Up').click()
+
+})
+
+  it('Email already exists', function(){
+
+    // Fill the agency name field 
+ 
+  cy.get('input[name="sign-up-agency-organization-name"]').focus().type('Agency Test proposes')
+
+  // Fill first name field 
+
+  cy.get('input[name="sign-up-agency-first-name"]').focus().type('Birdie')
+
+   // Fill last name field
+
+   cy.get('input[name="sign-up-agency-last-name"]').focus().type('Noyes')
+
+
+ // Fill Email and name field 
+  
+
+  cy.get('input[name="sign-up-agency-email"]').focus().type('jessica.olivo@jobsity.io')
+
+  // Fill Phone field 
+  
+  cy.get('input[name="sign-up-agency-mobile-phone"]').focus().type('972-519-5939')
+
+  // Fill Address field 
+  cy.get('input[name="sign-up-agency-address"]').focus().type('2666  Wilson Avenue')
+
+  // Fill city field 
+  cy.get('input[name="sign-up-agency-city"]').focus().type('Plano')
+
+  // Select a state from the list 
+
+  cy.select('.form-group','State','Texas')
+
+  // Fill Zip field 
+  cy.get('input[name="sign-up-agency-zip"]').focus().type('75075')
+
+
+  // Fill password field 
+  cy.get('input[name="sign-up-agency-password"]').focus().type('test123')
+
+  // Fill confirm password field 
+  cy.get('input[name="sign-up-agency-confirm-password"]').focus().type('test123')
+
+
+  // Check the agree terms  
+
+  cy.get('input[name="sign-up-agency-terms-accepted"]').check() 
+
+  // Click on the submit button 
+
+  cy.contains('Submit').click()
+
+  cy.get('#toast-container').should('contain.text','OopsEmail already exists..')  
+
+  })
 
 })
